45a4d6c9df0fe62ca5b439a499e364bb5e4ea1e8
Wiaam Suleiman <wiaam_k@hotmail.com>
2021-09-07 17:30:39 -0400

    game scene setup

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private Text _highScore = null;
+    [SerializeField] private Text _versionNumber = null;
     // Start is called before the first frame update
     void Start()
     {
-        _highScore.text = PlayerPrefs.HasKey("highScore") ? PlayerPrefs.GetInt("highScore").ToString() : 0.ToString();
+        _highScore.text = SaveDataController.Instance.MyData.Record_High_Score.ToString();
+        _versionNumber.text = SaveDataController.Instance.MyData.Version_Number.ToString();
+    }
+
+    public void OnStartButtonPressed()
+    {
+        SceneManager.LoadScene("GameScene");
     }
 
     // Update is called once per frame
